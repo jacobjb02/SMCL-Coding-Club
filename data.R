fa9bd6cf0394ce58d9fbc95725521ed7db87ab2a
Jacob Boulrice <jacobjboulrice@gmail.com>
2025-02-03 10:46:41 -0500

    load data

---
+++ data.R
@@ -0,0 +1,17 @@
+getData <- function() {
+  
+  Reach::downloadOSFdata(repository  = 'm5dt4',
+                         filelist    = list('data/' = c('control_aligned.zip',
+                                                        'control_rotated.zip',
+                                                        'handview_aligned.zip',
+                                                        'handview_rotated.zip',
+                                                        'cursorjump_aligned.zip',
+                                                        'cursorjump_rotated.zip',
+                                                        'demographics.csv'
+                         )  ),
+                         folder      = 'data/',
+                         overwrite   = TRUE,
+                         unzip       = TRUE,
+                         removezips  = TRUE)
+  
+}
